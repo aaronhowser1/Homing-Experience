d2dd1acfde9cfaf886f481371fbd833701a7278a
Aaron Howser <aaronhowser1@gmail.com>
2024-03-09 22:11:14 -0600

    im so ashamed

--- src/main/kotlin/dev/aaronhowser/homingexperience/event/ModCommonEvents.kt
+++ src/main/kotlin/dev/aaronhowser/homingexperience/event/ModCommonEvents.kt
@@ -5,6 +5,7 @@ import dev.aaronhowser.homingexperience.entity.HomingExperienceEntity
 import dev.aaronhowser.homingexperience.util.ModScheduler
 import net.minecraft.world.entity.ExperienceOrb
 import net.minecraftforge.event.TickEvent
+import net.minecraftforge.event.TickEvent.ServerTickEvent
 import net.minecraftforge.event.entity.EntityJoinLevelEvent
 import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.fml.common.Mod
@@ -25,9 +26,8 @@ object ModCommonEvents {
     }
 
     @SubscribeEvent
-    fun serverTick(event: TickEvent) {
+    fun serverTick(event: ServerTickEvent) {
         if (event.side.isClient) return
-        if (event.type != TickEvent.Type.SERVER) return
         if (event.phase == TickEvent.Phase.END) ModScheduler.tick++
     }
 
